9e0f44b03e67294bf5edf9513acacba19b7f60db
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2019-10-13 10:06:39 -0400

    css added

--- src/App.js
+++ src/App.js
@@ -13,20 +13,23 @@ class App extends React.Component {
     };
   }
 
-componentDidMount() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(users => this.setState({ monsters: users}));
-}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+  }
+
+  handleChange = (e) => {this.setState({ searchField: e.target.value })};
 
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
+        <h1>Monsters Rolodex </h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })} />
+          handleChange={ this.handleChange} />
       <CardList monsters={filteredMonsters} />
       </div>
     );
